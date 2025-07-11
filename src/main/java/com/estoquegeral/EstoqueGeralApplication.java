07aa3fcd084e1a473680e5411e4ed78a6d454cb7
Savio Sales <saviosales18@gmail.com>
2025-07-11 07:58:27 -0300

    Update EstoqueGeralApplication.java

--- src/main/java/com/estoquegeral/EstoqueGeralApplication.java
+++ src/main/java/com/estoquegeral/EstoqueGeralApplication.java
@@ -1,13 +1,13 @@
 package com.estoquegeral;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstoqueGeralApplication implements CommandLineRunner {
+public class EstoqueGeralApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstoqueGeralApplication.class, args);
 	}
+	
 } 
